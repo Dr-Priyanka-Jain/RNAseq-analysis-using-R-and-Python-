fd9b9aa631f609443d342ca065f5344f930bd2ff
Priyanka Thareja <98804219+priyanka-thareja@users.noreply.github.com>
2024-08-06 15:28:43 +0530

    Update RNAseq_analysis_using_Rsubread.R

--- R/RNAseq_analysis_using_Rsubread.R
+++ R/RNAseq_analysis_using_Rsubread.R
@@ -22,6 +22,12 @@ fc <- featureCounts(files=bamfiles,annot.inbuilt="mm10")
 names(fc)
 fc$stat
 head(fc$annotation)
+write.csv(fc$counts, file = "path/to/your/directory")
+
+write.csv(fc$annotation, file = "path/to/your/directory")
+
+write.csv(fc$targets, file = "path/to/your/directory")
+
 sampleInfo <- read.table("sample_info.csv",header=TRUE,sep=",",row.names=1)
 
 #Differential Gene Expression Analysis
@@ -49,46 +55,3 @@ https://www.nathalievialaneix.eu/doc/html/solution_edgeR-tomato-withcode.html
 
 
 
-***************************************************************
-
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install(org.Mm.eg.db)
-BiocManager::install("Rsubread")
-
-library(Rsubread)
-library(limma)
-library(edgeR)
-buildindex(basename="chr1_mm10",reference="chr1.fa")
-
-#########Alignment for multiple files
-reads1 <- list.files(path=".",pattern = "*.fastq.gz$")
-#reads2 <- list.files(path=".",pattern = "_R2_.*.fq$" )
-#all.equal(length(reads1),length(reads2))
-
-align(index="chr1_mm10", readfile1=reads1,input_format="FASTQ",output_format="BAM")
-#align(index="chr1_mm10",readfile1=targets$InputFile,output_file=targets$OutputFile)
-
-bamfiles <-list.files(path=".",pattern = "*.BAM$")
-fc <- featureCounts(files=bamfiles,annot.inbuilt="mm10")
-names(fc)
-fc$stat
-head(fc$annotation)
-sampleInfo <- read.table("sample_info",header=TRUE,sep=",",row.names=1)
-dgeFull <-DGEList(counts=fc$counts, gene=fc$annotation[,c("GeneID","Length")],group=sampleInfo$condition)
-
-dgeFull <- DGEList(dgeFull$counts[apply(dgeFull$counts, 1, sum) != 0, ],
-                   group=dgeFull$samples$group)
-head(dgeFull$counts)
-dgeFull <- calcNormFactors(dgeFull, method="TMM")
-dgeFull$samples
-head(dgeFull$counts)
-eff.lib.size <- dgeFull$samples$lib.size*dgeFull$samples$norm.factors
-normCounts <- cpm(dgeFull)
-pseudoNormCounts <- log2(normCounts + 1)
-#boxplot(pseudoNormCounts, col="gray", las=3)
-plotMDS(pseudoNormCounts)
-dgeFull <- estimateCommonDisp(dgeFull)
-dgeFull <- estimateTagwiseDisp(dgeFull)
-dgeFull
